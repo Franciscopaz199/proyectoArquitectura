b4a99eebabeb1b25f7eb433df7a894493cf26273
franciscopaz199 <fjpazf@unah.hn>
2023-10-21 19:22:10 -0600

    logo agregado

--- src/components/LayoutComponents/Header.jsx
+++ src/components/LayoutComponents/Header.jsx
@@ -5,15 +5,40 @@ import React from "react";
 function Header(props) {
      return (
           <>
-               <div className="flex p-4  sticky top-0 p-4 sm:w-3/4 mx-auto bg-white sm:justify-between justify-center items-center">
-                    <h2 className="text-blue-900 text-2xl font-bold">{ props.title }</h2>
-                    <ul className="flex space-x-4">
-                         <li className="text-blue-900 text-xl font-bold">{ props.subtitle }</li>
-                         <li className="text-blue-900 text-xl font-bold">{ props.subtitle2 }</li>
-                         <li className="text-">acerca de</li>
-                    </ul>
-                    {props.children}
-               </div>
+               <header class="flex  sticky top-0 flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+                    <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+                         <div class="flex items-center justify-between">
+                              <a class="inline-flex items-center gap-x-2 text-xl font-semibold dark:text-white" href="#">
+                              <div style={{
+                                   "background-image": "url('https://ui-avatars.com/api/?name=A+M&color=FFFFFF&background=09090b')",
+                              }} class="fi-avatar bg-cover bg-center h-9 w-9 fi-user-avatar "></div>
+                                   {props.title}
+                              </a>
+                              <div class="sm:hidden">
+                                   <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" data-hs-collapse="#navbar-image-and-text-1" aria-controls="navbar-image-and-text-1" aria-label="Toggle navigation">
+                                        <svg class="hs-collapse-open:hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                                        </svg>
+                                        <svg class="hs-collapse-open:block hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                        </svg>
+                                   </button>
+                              </div>
+                         </div>
+                         <div id="navbar-image-and-text-1" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
+                              <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
+                                   <a class="font-medium text-blue-500" href="#" aria-current="page">Calculadoras</a>
+                                   <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">{props.subtitle}</a>
+                                   <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">{props.subtitle}</a>
+                                   <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">{props.subtitle2}</a>
+                                   <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">acercade</a>
+                              </div>
+                         </div>
+                    </nav>
+               </header>
+
+
+              
 
           </>
      );
